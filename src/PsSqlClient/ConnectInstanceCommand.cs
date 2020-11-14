3531c00fc189c72958094220ee2132bd6eebd985
Steffen Kampmann <steffen.kampmann@gmail.com>
2020-11-14 11:01:39 +0100

    Added connect by connection string

---
+++ src/PsSqlClient/ConnectInstanceCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Data.SqlClient;
+
+namespace PsSqlClient
+{
+    [Cmdlet(VerbsCommunications.Connect, "Instance")]
+    [OutputType(typeof(SqlConnection))]
+    public class ConnectInstanceCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        [ValidateNotNullOrEmpty()]
+        public string ConnectionString { get; set; }
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin!");
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            WriteObject(new SqlConnection(ConnectionString));
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End!");
+        }
+    }
+}

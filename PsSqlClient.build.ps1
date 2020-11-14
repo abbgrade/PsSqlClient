4af00111d32724916e0efd017c49856f6a7230d5
Steffen Kampmann <steffen.kampmann@gmail.com>
2020-11-14 15:25:23 +0100

    Update pester

--- PsSqlClient.build.ps1
+++ PsSqlClient.build.ps1
@@ -28,12 +28,24 @@ task test requireTestDependencies, build, {
 # Synopsis: Install the dependencies for tests.
 task requireTestDependencies {
 
-	if ( -not ( ( Get-Module -ListAvailable -Name Pester ).Version.Major -ge 5 )) {
+	if ( -not ( Get-Module -ListAvailable -Name Pester )) {
 		Install-Module Pester -Scope CurrentUser -SkipPublisherCheck
+	} elseif ( ( Get-Module -ListAvailable -Name Pester ).Version.Major -lt 5) {
+		Update-Module Pester -Scope CurrentUser -SkipPublisherCheck
 	}
 	if ( -not ( Get-Module -ListAvailable -Name PSDocker )) {
 		Install-Module PSDocker -Scope CurrentUser
 	}
+}
+
+# Synopsis: Install the dependencies without installing the module.
+task installDependencies {
+	Find-Package -ProviderName NuGet -Name System.Data.SqlClient | Install-Package
+}
+
+task checkDependencies {
+	# Install-Module -Name Gac -Scope CurrentUser
+	Get-GacAssembly -Name System.Data.SqlClient
 }
 
 # Synopsis: Default task.

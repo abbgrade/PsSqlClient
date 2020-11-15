3e2ad0cdab580f9b098fcd775eecb0e0242ab63a
Steffen Kampmann <steffen.kampmann@gmail.com>
2020-11-15 12:39:01 +0100

    Added verbose output

--- src/PsSqlClient/ConnectInstanceCommand.cs
+++ src/PsSqlClient/ConnectInstanceCommand.cs
@@ -1,38 +1,38 @@
 ﻿using System;
 using System.Management.Automation;
-using System.Management.Automation.Runspaces;
 using System.Data.SqlClient;
 
 namespace PsSqlClient
 {
-    [Cmdlet(VerbsCommunications.Connect, "Instance")]
+    [Cmdlet(VerbsCommunications.Connect, "Instance", DefaultParameterSetName = "ConnectionString")]
     [OutputType(typeof(SqlConnection))]
     public class ConnectInstanceCommand : PSCmdlet
     {
         [Parameter(
-            Mandatory = true,
+            ParameterSetName = "ConnectionString",
             Position = 0,
+            Mandatory = true,
             ValueFromPipeline = true,
             ValueFromPipelineByPropertyName = true)]
         [ValidateNotNullOrEmpty()]
         public string ConnectionString { get; set; }
 
-        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
-        protected override void BeginProcessing()
-        {
-            WriteVerbose("Begin!");
-        }
-
-        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
-            WriteObject(new SqlConnection(ConnectionString));
+            switch (ParameterSetName)
+            {
+                case "ConnectionString":
+                    WriteVerbose("Connect by connection string");
+                    var connection = new SqlConnection(ConnectionString);
+                    connection.Open();
+                    WriteVerbose($"Connection to {connection.DataSource} is {connection.State}");
+                    WriteObject(connection);
+                    break;
+
+                default:
+                    throw new NotImplementedException($"ParameterSetName {ParameterSetName} is not implemented");
+            }
         }
 
-        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
-        protected override void EndProcessing()
-        {
-            WriteVerbose("End!");
-        }
     }
 }

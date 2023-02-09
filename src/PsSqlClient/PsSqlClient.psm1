b4d4d6feb3a42582332a17d6cb8d62a8a7c141a9
Andreas Westhoff <awesthoff58@web.de>
2023-02-09 10:23:44 +0100

    added check if assembly is already loaded

--- src/PsSqlClient/PsSqlClient.psm1
+++ src/PsSqlClient/PsSqlClient.psm1
@@ -1,4 +1,7 @@
-Add-Type -Path $PSScriptRoot\runtimes\win\lib\netcoreapp3.1\Microsoft.Data.SqlClient.dll
+$assemblyLoaded = [System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object Location -like "*Microsoft.Data.SqlClient.dll"
+if ([string]::IsNullOrEmpty($assemblyLoaded)) {
+    Add-Type -AssemblyName $PSScriptRoot\runtimes\win\lib\netcoreapp3.1\Microsoft.Data.SqlClient.dll
+}
 Add-Type -Path $PSScriptRoot\Microsoft.Identity.Client.dll
 Add-Type -Path $PSScriptRoot\Azure.Core.dll
 Add-Type -Path $PSScriptRoot\Azure.Identity.dll

d93bc2e3c46bbce1e5a1b97100a03e29c5a56893
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-02-14 13:13:51 +0100

    added output to dll loading

--- src/PsSqlClient/PsSqlClient.psm1
+++ src/PsSqlClient/PsSqlClient.psm1
@@ -1,5 +1,20 @@
-Add-Type -Path $PSScriptRoot\runtimes\win\lib\netstandard2.1\Microsoft.Data.SqlClient.dll
-Add-Type -Path $PSScriptRoot\Microsoft.Identity.Client.dll
-Add-Type -Path $PSScriptRoot\Azure.Core.dll
-Add-Type -Path $PSScriptRoot\Azure.Identity.dll
-Add-Type -Path $PSScriptRoot\Microsoft.SqlServer.Server.dll
+$LoadedAssemblies = [System.AppDomain]::CurrentDomain.GetAssemblies()
+
+@(
+    "$PSScriptRoot\runtimes\win\lib\netstandard2.1\Microsoft.Data.SqlClient.dll",
+    "$PSScriptRoot\Microsoft.Identity.Client.dll",
+    "$PSScriptRoot\Azure.Core.dll",
+    "$PSScriptRoot\Azure.Identity.dll",
+    "$PSScriptRoot\Microsoft.SqlServer.Server.dll"
+) | ForEach-Object {
+    $RequiredAssemblyPath = $_
+    $LoadedAssembly = $LoadedAssemblies | Where-Object Location -Like "*$( $RequiredAssemblyPath.Name )"
+
+    if ( $SqlClientAssembly ) {
+        Write-Warning "Assembly '$( $LoadedAssembly.GetName() )' already loaded from '$( $LoadedAssembly.Location )'. Skip adding defined dll."
+    }
+    else {
+        Write-Verbose "Add assembly '$( $RequiredAssemblyPath.Name )'"
+        Add-Type -Path $RequiredAssemblyPath
+    }
+}
